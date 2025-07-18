99ea91fcc89cf2a710d675ff98753607f19cf1c6
MambaCreatives <alvinkiptoo26@gmail.com>
2025-07-18 22:11:11 +0300

    changed email

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -138,10 +138,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'alvinkiptoo26@gmail.com'
 EMAIL_HOST_PASSWORD = 'edjp ipgy ifaw dsyq'  # Not your Gmail login password!
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER'''
